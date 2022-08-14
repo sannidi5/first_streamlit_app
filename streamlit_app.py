3984ae9460715c8f370fa7470495309dd6b9c100
sannidi <111211735+sannidi5@users.noreply.github.com>
2022-08-13 22:38:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("Healthy Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Breakfast 1")
-streamlit.text("Breakfast 2")
-
+streamlit.text("🥣 Breakfast 1")
+streamlit.text("🥗 Breakfast 2")
+streamlit.text("🐔 Breakfast 3")
+streamlit.text("🥑🍞Breakfast 4")
