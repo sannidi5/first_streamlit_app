ccb703269c906943d89658ac3aec3638de1632c6
sannidi <111211735+sannidi5@users.noreply.github.com>
2022-08-15 18:20:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Healthy Dinner")
 streamlit.header("Breakfast Menu")
@@ -7,3 +8,6 @@ streamlit.text("🥗 Breakfast 2")
 streamlit.text("🐔 Breakfast 3")
 streamlit.text("🥑🍞Breakfast 4")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
