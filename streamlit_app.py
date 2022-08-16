d5cdfb9b0645ab342c32b21a37edacde54de43d1
sannidi <111211735+sannidi5@users.noreply.github.com>
2022-08-16 15:53:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas
 import requests
 import snowflake.connector
 
+from urllib.error import URLError
+
 streamlit.title("Healthy Dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Breakfast 1")
@@ -36,6 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 my_cur = my_cnx.cursor()
